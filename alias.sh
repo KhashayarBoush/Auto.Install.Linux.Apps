f318cdc62bdcba0f7587e01993b596baa280b8f2
khashayarboush <khashayar.boush@icoud.com>
2021-02-01 20:12:40 +0330

    Add new Package to instal file

--- alias.sh
+++ alias.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "\nalias install='sudo apt-get install'\nalias cls='clear'\nalias update='sudo apt-get update;sudo apt-get upgrade'\nalias ..='cd ..'\nalias grep='grep --color=auto'\nalias ls='ls --color=auto'\nalias ll='ls -l --color=auto'\nalias ls='ls --color=auto'\nalias rm='rm -i'\n" >> ~/.bashrc
+echo -e "\nalias install='sudo apt-get install'\nalias cls='clear'\nalias update='sudo apt-get update;sudo apt-get upgrade'\nalias ..='cd ..'\nalias grep='grep --color=auto'\nalias ls='ls --color=auto'\nalias ll='ls -l --color=auto'\nalias ls='ls --color=auto'\nalias rm='rm -i'\n" >> ~/.zsh.rc
 
-source ~/.bashrc
+#source ~/.zshrc
--- apps.sh
+++ apps.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 #sudo apt-get install unity-tweak-tool gnome-tweak-tool
-sudo apt install vim axel links aria2 lynx python python3 python-pip3 python3-pip leafpad nload htop iftop net-tools -y 
+sudo apt install vim axel links aria2 lynx python python3 python3-pip nload htop iftop net-tools -y 
 sudo apt-get install gdebi -y
 #sudo apt-get install unity-webapps-common unity-tweak-tool
 sudo apt-get install tree mc cmus terminator tilix git axel links aria2 lynx -y 
-sudo apt-get install at vlc rsync python python3 python3-pip python-pip leafpad -y
+sudo apt-get install at vlc rsync python python3 python3-pip python-pip  -y
 sudo apt-get install virtualbox -y 
 sudo apt install -y openvpn dialog python3-pip python3-setuptools
 sudo pip3 install protonvpn-cli
 sudo apt-get install tor
 sudo pip3 install -U django
-sudo pip3 insatll -U ipython
+sudo pip3 install -U ipython
+sudo pip3 install -U jupyter
+sudo pip3 install -U virtualenv
 
-
-/home/khashayarboush/Shell/apps.sh
+sudo apt-get install stacer zsh mc tilix openssh-server openssh-client 
+sudo apt-get install openvpn wireguard tmux links aria2 lynx axel htop
+sudo apt-get install iftop tcpdump wireshark vim gedit nano mc nautilus 
+sudo apt-get install vlc kazam python3 python python3-pip youtube-dl rsync 
+sudo apt-get install tor gparted net-tools vsftpd zip unzip rar unrar openvpn 
+sudo apt-get install openssl nmap virtualbox gimp php libapache2-mod-php apache2
+sudo apt-get install tmux netcat aircrack-ng macchanger git cmatrix curl wget 
+sudo apt-get install slurm php libapache2-mod-php
+sudo apt-get install php-mysql php-curl php-json php-gd php-zip 
+sudo apt-get install php-mbstring apache2 php libapache2-mod-php mysql-server 
+sudo apt-get install php-mysql 
