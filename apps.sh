1895d5c1d037f26db4e4ec44a939251c33bfa44f
khashayarboush <khashayar.boush@icoud.com>
2021-04-04 21:00:41 +0430

    Add new package

--- apps.sh
+++ apps.sh
@@ -15,17 +15,36 @@ sudo pip3 install -U ipython
 sudo pip3 install -U jupyter
 sudo pip3 install -U virtualenv
 
-sudo apt-get install renger trash-cli neofetch ncdu npm nodejs node
-sudo apt-get install hollywood xterm fortune cowsay lolcat xcowsay
-sudo apt-get install figlet toilet
+sudo apt-get install renger trash-cli neofetch ncdu npm nodejs node -y
+
+sudo apt-get install hollywood xterm fortune cowsay lolcat xcowsay -y
+
+sudo apt-get install figlet toilet -y
+
 sudo apt-get install stacer zsh mc tilix openssh-server openssh-client 
-sudo apt-get install openvpn wireguard tmux links aria2 lynx axel htop
-sudo apt-get install iftop tcpdump wireshark vim gedit nano mc nautilus 
-sudo apt-get install vlc kazam python3 python python3-pip youtube-dl rsync 
-sudo apt-get install tor gparted net-tools vsftpd zip unzip rar unrar openvpn 
-sudo apt-get install openssl nmap virtualbox gimp php libapache2-mod-php apache2
-sudo apt-get install tmux netcat aircrack-ng macchanger git cmatrix curl wget 
-sudo apt-get install slurm php libapache2-mod-php
-sudo apt-get install php-mysql php-curl php-json php-gd php-zip 
-sudo apt-get install php-mbstring apache2 php libapache2-mod-php mysql-server 
-sudo apt-get install php-mysql mplayer
+-y 
+sudo apt-get install openvpn wireguard tmux links aria2 lynx axel htop -y
+
+sudo apt-get install iftop tcpdump wireshark vim gedit nano mc nautilus -y
+
+sudo apt-get install vlc kazam python3 python python3-pip youtube-dl rsync -y
+
+sudo apt-get install tor gparted net-tools vsftpd zip unzip rar unrar openvpn -y
+
+sudo apt-get install openssl nmap virtualbox gimp php libapache2-mod-php apache2 -y
+
+sudo apt-get install tmux netcat aircrack-ng macchanger git cmatrix curl wget -y 
+
+sudo apt-get install slurm php libapache2-mod-php -y
+
+sudo apt-get install php-mysql php-curl php-json php-gd php-zip -y
+
+sudo apt-get install php-mbstring apache2 php libapache2-mod-php mysql-server -y 
+
+sudo apt-get install php-mysql mplayer -y
+
+sudo apt-get install lm-sensors -y 
+
+sudo apt install hardinfo -y 
+
+
